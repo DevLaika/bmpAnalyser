a9204e482aaf6073a4ccfe0b4de82733b98ab289
Laika <oleksnelder@gmail.com>
2019-09-23 20:59:52 +0200

    final byte

--- main.js
+++ main.js
@@ -4,18 +4,17 @@ import bmpJs from 'bmp-js'
 const bmpBuffer = fs.readFileSync('maskTemplate.bmp')
 const bmpData = bmpJs.decode(bmpBuffer)
 
-fs.writeFileSync('data.txt', bmpData.data);
 
-// // Create new colorList array with amount of pixels
-// let colorList = new Array(bmpData.data.length / 4)
-// // Insert colors into colorList by reading bmp data
-// for (let i = 0; i < colorList.length; i++) {
-//     let newColor = []
-//     for (let j = 0; j < 3; j++) { // Grab only the last 3 RGB values for the pixel (first value is alpha, to grab alpha as well back change 3 --> 4 and remove the +1 from the next line)
-//         newColor[j] = bmpData.data[i*4+j+1]
-//     }
-//     colorList[i] = newColor
-// }
+// Create new colorList array with amount of pixels
+let colorList = '';
+// Insert colors into colorList by reading bmp data
+for (let i = 0; i < bmpData.data.length / 4; i++) {
+    for (let j = 0; j < 3; j++) { // Grab only the last 3 RGB values for the pixel (first value is alpha, to grab alpha as well back change 3 --> 4 and remove the +1 from the next line)
+        colorList += String.fromCharCode(bmpData.data[i*4+j+1])
+    }
+}
+
+fs.writeFileSync('data.txt', colorList);
 
 // // Create new colorMatrix array with image height and width
 // let colorMatrix = new Array(bmpData.height)
