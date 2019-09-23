9b5043b51d24211484314a22b9fd3d48c017ec41
Laika <oleksnelder@gmail.com>
2019-09-23 21:12:38 +0200

    commented

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-import fs from 'fs'
-import bmpJs from 'bmp-js'
+import fs from 'fs' // Use fs package
+import bmpJs from 'bmp-js' // Use bmp-js package
 
-const bmpBuffer = fs.readFileSync('maskTemplate.bmp')
-const bmpData = bmpJs.decode(bmpBuffer)
+const bmpBuffer = fs.readFileSync('maskTemplate.bmp') // Import bmp image
+const bmpData = bmpJs.decode(bmpBuffer) // Decode bmp image to bmpData
 
 
-// Create new colorValues array with amount of pixels
-let colorValues = '';
+let colorValues = ''; // Create eventual byte variable where color values will be stored
 // Insert colors into colorValues by reading bmp data
-for (let i = 0; i < bmpData.data.length / 4; i++) {
-    for (let j = 0; j < 3; j++) { // Grab only the last 3 RGB values for the pixel (first value is alpha, to grab alpha as well back change 3 --> 4 and remove the +1 from the next line)
-        colorValues += String.fromCharCode(bmpData.data[i*4+j+1])
+for (let i = 0; i < bmpData.data.length / 4; i++) { // For every four values (for every pixel in the image)
+    for (let j = 0; j < 3; j++) { // Repeat 3 times with i increasing every time
+        colorValues += String.fromCharCode(bmpData.data[i*4+j+1]) // Grab only the last 3 RGB values for the pixel (first value is alpha, unnececary for our purposes)
     }
 }
 
-fs.writeFileSync('data.txt', colorValues);
+fs.writeFileSync('data.txt', colorValues); // Write the byets to data.txt
